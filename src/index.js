e2a341e6ce92887419230df690b28b885af3f727
Saatvik K <skambhampati24@gmail.com>
2021-10-27 19:04:54 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const logger = require("morgan");
+const Invalid = "Invalid Option";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger("dev"));
+
+app.set("view engine", "ejs");
+
+app.listen(port, function() {
+  console.log("Server running on port " + port);
+});
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
